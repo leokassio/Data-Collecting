0df4e4b20d6aa6c4436bec2da40ddc764c7767ef
Kassio Machado <kassio.comp@gmail.com>
2015-10-28 13:19:57 -0400

    add the feature to collect precise, geocoded or none locations on twitter monitor based on hashtags

--- twitter-monitor-hashtags.py
+++ twitter-monitor-hashtags.py
@@ -32,6 +32,14 @@ def loadHashtags(configparser):
 	tags = configparser.get('twitter_oauth', 'hashtags')
 	return tags.split(',')
 
+def loadGeolocatedMethod(configparser):
+	gfm = configparser.getint('twitter_oauth', 'geolocated_filter_method')
+	return gfm
+
+def loadFilenameAlias(configparser):
+	fa = configparser.get('twitter_oauth', 'filename_alias')
+	return fa	
+
 if len(sys.argv) == 1:
 	print 'PLEASE INFORM THE INPUT DATA'
 	exit()
@@ -41,43 +49,47 @@ else:
 	ckey, csecret = loadAppOAuth(configparser)
 	atoken, asecret = loadUserOAuth(configparser, sys.argv[1])
 	hashtags = loadHashtags(configparser)
+	geolocated_filter_method = loadGeolocatedMethod(configparser)
+	filename_alias = loadFilenameAlias(configparser)
 
 
 print 'Using Profile ' + sys.argv[1] + ' to monitor hashtags'
 
-outputFile = open(datetime.datetime.now().strftime('%Y-%m-%d')+'-tweets-hashtags.csv', 'a', 0)
+outputFile = open(datetime.datetime.now().strftime('%Y-%m-%d')+'-tweets-' + filename_alias + '.csv', 'a', 0)
 
 class listener(StreamListener):
 	def on_data(self, data):
 		try:
 			parsed_json = json.loads(data)
+			if parsed_json['coordinates'] != None:
+				coords = parsed_json['coordinates']['coordinates']
+			else:
+				if geolocated_filter_method == 1: # flag for only precise geolocation method
+					return True
+				coords = [None, None]
+			if parsed_json['place'] != None:
+				location = parsed_json['place']['full_name'].encode('utf-8')
+				location = location.replace(',', ' ')
+			else:
+				if geolocated_filter_method == 2:
+					return True
+				location = 'None'
 			tweet = parsed_json['text'].replace('\n', '').encode('utf-8')
 			tweet = tweet.replace(',', ' ')
 			tid = str(parsed_json['id']).encode('utf-8')
 			uid = str(parsed_json['user']['id']).encode('utf-8')
 			timestamp = parsed_json['created_at'].replace('\t', '').encode('utf-8')
-                        timestamp = datetime.datetime.strptime(timestamp, '%a %b %d %H:%M:%S +0000 %Y')
-                        timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-			if parsed_json['coordinates'] != None:
-				coords = parsed_json['coordinates']['coordinates']
-			else:
-				coords = [None, None]
-				return True ################################################# JUST GEOLOCATED TWEETS
-			if parsed_json['place'] != None:
-				location = parsed_json['place']['full_name'].encode('utf-8')
-				location = location.replace(',', ' ')
-			else:
-				location = 'None'
+			timestamp = datetime.datetime.strptime(timestamp, '%a %b %d %H:%M:%S +0000 %Y')
+			timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
 			if parsed_json['source'] != None:
 				source = parsed_json['source'].encode('utf-8')
 			else:
 				source = 'None'
 			msg = tid + ', ' + uid + ', ' + str(coords[0]).encode('utf-8') + ', ' + str(coords[1]).encode('utf-8') + ', ' + timestamp + ', ' + location + ', ' + tweet + ', ' + source
-                        if 'insta' in source:
-                                print colorama.Fore.YELLOW, msg, colorama.Fore.RESET
-                        else:
-                                print msg
-
+			if 'insta' in source:
+				print colorama.Fore.YELLOW, msg, colorama.Fore.RESET
+			else:
+				print msg
 			outputFile.write(msg+'\n')
 		except KeyError:
 			if 'limit' not in parsed_json.keys():
