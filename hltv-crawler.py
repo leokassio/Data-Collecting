82a315f99b816337b37f8e9fffceed7f249a204e
Kassio Machado <kassio.comp@gmail.com>
2016-03-14 10:18:10 -0400

    big changes on crawler. Migration from selenium to lxml!

--- hltv-crawler.py
+++ hltv-crawler.py
@@ -9,66 +9,51 @@
 
 import sys
 import time
+import httplib
+import urllib2
 import colorama
 import datetime
+from lxml.etree import XMLSyntaxError
 from tqdm import tqdm
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver import PhantomJS
-
-def createDriver():
-	try:
-		# driver = Firefox()
-		driver = PhantomJS('./phantomjs')
-	except:
-		driver = PhantomJS()
-	return driver
+from HTMLParser import HTMLParser
+from lxml import html
 
 def main():
+	minutes = 15
 	fileout = open('hltv-stream.csv', 'a', 0)
 	url = 'http://www.hltv.org'
-	minutes = 2
-	driver = createDriver()
-	driver.set_page_load_timeout(30)
-	while True:
-		timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:00')
-		try:
-			driver.get(url)
-			elements = driver.find_elements_by_id('secondCollumn')
-			for e in elements:
-				for i, t in enumerate(e.find_elements_by_id('boc1')):
-					if i != 1: continue
-					stream = [a.get_attribute('title') for a in t.find_elements_by_tag_name('a')]
-					href = [a.get_attribute('href') for a in t.find_elements_by_tag_name('a')]
-					viewers = [v.text for v in t.find_elements_by_tag_name('div')[4:-1:2]]
-					kind = [img.get_attribute('src') for img in t.find_elements_by_tag_name('img')[1::2]]
-					country = [img.get_attribute('src') for img in t.find_elements_by_tag_name('img')[2::2]]
-					for s, v, h, k, c in zip(stream, viewers, href, kind, country):
-						v = v.replace('(', '').replace(')', '')
-						k = k.replace('http://static.hltv.org//images/', '')
-						c = c.replace('http://static.hltv.org//images/', '')
-						pcolor = colorama.Fore.CYAN
-						if c == 'flag/br.gif':
-							pcolor = colorama.Fore.YELLOW
-						elif k == 'mod_csgo_caster.png':
-							pcolor = colorama.Fore.RED
-						elif k == 'mod_csgo_player.png':
-							pcolor = colorama.Fore.BLUE
-						elif k == 'mod_csgo_female.png':
-							pcolor = colorama.Fore.MAGENTA
-						else:
-							pcolor = colorama.Fore.WHITE
+	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+	'Accept-Encoding': 'none',
+	'Accept-Language': 'en-US,en;q=0.8',
+	'Connection': 'keep-alive'}
 
-						print pcolor + timestamp, s, v, h, k, c, colorama.Fore.RESET + colorama.Back.RESET	
-						fileout.write(timestamp + ',' + s + ',' + v + ',' + h + ',' + k + ',' + c + '\n')
-		except TimeoutException:
-			print colorama.Back.RED + colorama.Fore.WHITE + 'Error on loading ' + url + colorama.Fore.RESET + colorama.Back.RESET + ' - waiting 1 minute to try again'
+	while True:
+		timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		try:
+			req = urllib2.Request(url, headers=hdr)
+			page = urllib2.urlopen(req)
+			content = page.read()
+			tree = html.fromstring(content)
+			elmts = tree.get_element_by_id('secondCollumn')	
+			for ei, ej in zip(elmts[1][1][0:-1:3], elmts[1][1][1:-1:3]): # secondCollumn>>boc1 (second element)>>BoxDodyFadeDark(second element)
+				stream = ei.getchildren()[0].items()[0][1]
+				viewers = int(ej.text.replace('(', '').replace(')', ''))
+				streamid = ei.getchildren()[0].items()[1][1][2:]
+				kind = ei.getchildren()[0].getchildren()[0].items()[2][1].replace(' ', '-')
+				country = ei.getchildren()[0].getchildren()[1].items()[0][1].replace('http://static.hltv.org//images/', '')
+				line = timestamp + ',' + stream + ',' + str(viewers) + ',' + country + ',' + kind + ',' + streamid
+				print line
+				fileout.write(line + '\n')
+			for i in tqdm(range(15 * 60), desc='waiting ' + str(minutes) + 'mins'):
+				time.sleep(1)
+		except lxml.etree.XMLSyntaxError:
+			print 'lxml.etree.XMLSyntaxError, waiting 1 minute....'
 			time.sleep(60)
-			continue
-		print colorama.Fore.YELLOW
-		for i in tqdm(range(60 * minutes), desc='Waiting ' + str(minutes) + ' mins'):
-			time.sleep(1)
-		print colorama.Fore.RESET
-			
+		except:
+			print 'Not Threated Except, waiting 1 minute...'
+			time.sleep(60)	
 
 if __name__ == "__main__":
 	main()
