be6017aed15f94a11144d5835e0df68442232ab1
Kassio Machado <kassio.comp@gmail.com>
2016-03-15 10:15:01 -0400

    adding prettytable module for a better presentation on console

--- hltv-crawler.py
+++ hltv-crawler.py
@@ -13,14 +13,15 @@ import httplib
 import urllib2
 import colorama
 import datetime
-from lxml.etree import XMLSyntaxError
-from tqdm import tqdm
-from HTMLParser import HTMLParser
 from lxml import html
+from tqdm import tqdm
+from lxml.etree import XMLSyntaxError
+from HTMLParser import HTMLParser
+from prettytable import PrettyTable
 
 def main():
-	minutes = 15
-	fileout = open('hltv-stream.csv', 'a', 0)
+	minutes = 5
+	fileout = open('hltv-crawler-streams.csv', 'a', 0)
 	url = 'http://www.hltv.org'
 	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -31,6 +32,7 @@ def main():
 
 	while True:
 		timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		pttb = PrettyTable(['Time', 'Stream', 'Viewers', 'Country', 'Class'])
 		try:
 			req = urllib2.Request(url, headers=hdr)
 			page = urllib2.urlopen(req)
@@ -42,16 +44,21 @@ def main():
 				viewers = int(ej.text.replace('(', '').replace(')', ''))
 				streamid = ei.getchildren()[0].items()[1][1][2:]
 				kind = ei.getchildren()[0].getchildren()[0].items()[2][1].replace(' ', '-')
-				country = ei.getchildren()[0].getchildren()[1].items()[0][1].replace('http://static.hltv.org//images/', '')
+				country = ei.getchildren()[0].getchildren()[1].items()[0][1].replace('http://static.hltv.org//images/flag/', '')
+				country = country.replace('.gif', '').upper()
 				line = timestamp + ',' + stream + ',' + str(viewers) + ',' + country + ',' + kind + ',' + streamid
-				print line
 				fileout.write(line + '\n')
-			for i in tqdm(range(15 * 60), desc='waiting ' + str(minutes) + 'mins'):
+				pttb_data = [timestamp, stream, viewers, country, kind]
+				pttb.add_row(pttb_data)
+			print pttb
+			for i in tqdm(range(minutes * 60), desc='waiting ' + str(minutes) + 'mins'):
 				time.sleep(1)
 		except lxml.etree.XMLSyntaxError:
+			print pttb
 			print 'lxml.etree.XMLSyntaxError, waiting 1 minute....'
 			time.sleep(60)
 		except:
+			print pttb
 			print 'Not Threated Except, waiting 1 minute...'
 			time.sleep(60)	
 
